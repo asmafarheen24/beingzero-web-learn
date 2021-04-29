671a42d74ff10bb918de678077d7ff7afe68316d
Asma Farheen <asmafarheen1510@gmail.com>
2021-04-29 10:08:24 +0530

    added resume page response

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 app.get("/", function(req, res){
     res.send("Welcome to  Farheen's Basic Site");
 })
+app.get("/resume",funtion(req,res){
+    res.send("This is my resume page");
+})
  
 // Heroku will automatically set an environment variable called PORT
 const PORT = process.env.PORT || 3000;
