39a3bcd886f593d225af2d9577d2ab9fc774ba2b
Asma Farheen <asmafarheen1510@gmail.com>
2021-05-19 09:41:22 +0530

    curd password hardcord

--- testdb.js
+++ testdb.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const courselib=require('./backend/lib/courselib');
-var password = process.env.Mongo_atlas_password;
-var connectionString = "mongodb+srv://Farheen:"+password+"@cluster0.i9xkl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+// var password = process.env.Mongo_atlas_password;
+var connectionString = "mongodb+srv://Farheen:Shannudb3562@cluster0.i9xkl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
  
 mongoose.connect(connectionString, {useUnifiedTopology: true,useNewUrlParser: true});
 mongoose.connection.on('connected', function(){
